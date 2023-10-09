460fa320e4fdb9fdacca6db28565aad184d5e757
Karen Schick <karen.schick6@gmail.com>
2023-10-09 11:24:12 -0400

    commented out all

--- packages/client/src/hooks/useCurrency.js
+++ packages/client/src/hooks/useCurrency.js
@@ -1,57 +1,57 @@
-import React, { createContext, useContext, useReducer, useMemo } from "react";
+// import React, { createContext, useContext, useReducer, useMemo } from "react";
 
-const initialState = {
-  currency: "USD",
-  currencySymbol: "$",
-  multiplierFactor: 1,
-};
+// const initialState = {
+//   currency: "USD",
+//   currencySymbol: "$",
+//   multiplierFactor: 1,
+// };
 
-export const CurrencyContext = createContext(initialState);
+// export const CurrencyContext = createContext(initialState);
 
-CurrencyContext.displayName = "CurrencyContext";
+// CurrencyContext.displayName = "CurrencyContext";
 
-function currencyReducer(state, action) {
-  switch (action.type) {
-    case "SET_CURRENCY": {
-      return {
-        ...state,
-        currency: "EURO",
-        currencySymbol: "€",
-        multiplierFactor: 0.8,
-      };
-    }
+// function currencyReducer(state, action) {
+//   switch (action.type) {
+//     case "SET_CURRENCY": {
+//       return {
+//         ...state,
+//         currency: "EURO",
+//         currencySymbol: "€",
+//         multiplierFactor: 0.8,
+//       };
+//     }
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
 
-export const currencyProvider = (props) => {
-  const [state, dispatch] = useReducer(currencyReducer, initialState);
+// export const currencyProvider = (props) => {
+//   const [state, dispatch] = useReducer(currencyReducer, initialState);
 
-  const toggleCurrency = () => dispatch({ type: "SET_CURRENCY" });
+//   const toggleCurrency = () => dispatch({ type: "SET_CURRENCY" });
 
-  const value = useMemo(
-    () => ({
-      ...state,
-      toggleCurrency,
-    }),
-    [state]
-  );
+//   const value = useMemo(
+//     () => ({
+//       ...state,
+//       toggleCurrency,
+//     }),
+//     [state]
+//   );
 
-  return <CurrencyContext.Provider value={value} {...props} />;
-};
+//   return <CurrencyContext.Provider value={value} {...props} />;
+// };
 
-const useCurrency = () => {
-  const context = useContext(CurrencyContext);
-  if (context === undefined) {
-    throw new Error(`useCurrency must be used within a CurrencyProvider`);
-  }
-  return context;
-};
+// const useCurrency = () => {
+//   const context = useContext(CurrencyContext);
+//   if (context === undefined) {
+//     throw new Error(`useCurrency must be used within a CurrencyProvider`);
+//   }
+//   return context;
+// };
 
-export const ManagedCurrencyContext = ({ children }) => (
-  <currencyProvider>{children}</currencyProvider>
-);
+// export const ManagedCurrencyContext = ({ children }) => (
+//   <currencyProvider>{children}</currencyProvider>
+// );
 
-export default useCurrency;
+// export default useCurrency;
