b9e4f9fbe1048ce0a59525a9ff61f41ac8cf9e9b
Karen Schick <karen.schick6@gmail.com>
2023-10-11 20:47:16 -0400

    building coupon input search

--- packages/client/src/components/CartSummary/index.js
+++ packages/client/src/components/CartSummary/index.js
@@ -1,13 +1,43 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import {useCurrency} from "hooks/useCurrency";
+import { Container, Row, Col, Form, InputGroup, Button } from "react-bootstrap";
+//import { useCurrency } from "hooks/useCurrency";
+//import { useProvideCart } from "hooks";
+import useCurrency from "hooks/useCurrency";
 
 export default function CartSummary({ cartTotal }) {
   const { getPrice } = useCurrency();
+  // const { coupons, setCoupons} = 
+
+  // const handleSearchInputChange = (e) => {
+  //   e.preventDefault();
+  //   setCoupons(e.target.value);
+  // };
 
   return (
     <div className="cart-summary">
       <Container>
+        <Row className="mb-4">
+          <Col>
+            <InputGroup>
+              <Form.Control
+                type="text"
+                placeholder="Enter Coupon Code"
+                name="coupons"
+                //onChange={handleSearchInputChange}
+                //value={coupons}
+                minLength={1}
+                maxLength={30}
+              ></Form.Control>
+            </InputGroup>
+          </Col>
+          <Col className="my-auto">
+          <Button className="btn-sm " >
+            {" "}
+            Apply
+          </Button>
+        </Col>
+        </Row>
+        
         <Row className="mb-2 summary-item">
           <Col xs="9">
             <p className="summary-label">Free Shipping</p>
